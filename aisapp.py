be6fe4877e9e74fbf45b74830ad801e9322ff77b
Knut H Pedersen <knuthp@gmail.com>
2017-05-11 21:49:02 +0200

    Adding simple history display.

--- aisapp.py
+++ aisapp.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory, abort
 from flask_sqlalchemy import SQLAlchemy
 
-from aismodel import db, LastPositionReport
+from aismodel import db, LastPositionReport, HistoricPositionReport
 from aisschema import ma, LastPositionReportSchema
 
 
@@ -20,7 +20,7 @@ app = create_app()
 
 @app.route('/')
 def hRoot():
-    return "Hello"
+    return send_from_directory('static', 'leaflet.html')
 
 def _last_position_report_2_geojson(x):
     return {'type' : 'Feature', 
@@ -41,6 +41,20 @@ def aShips():
     tracks = LastPositionReport.query.all()
     geojson_features = [_last_position_report_2_geojson(x) for x in tracks]
     return jsonify({'type' : 'FeatureCollection', 'features' : geojson_features})
+
+@app.route('/api/tracks/<mmsi>')
+def aShip(mmsi):
+    track = LastPositionReport.query.filter(LastPositionReport.mmsi == int(mmsi)).first()
+    if track:
+        return jsonify(_last_position_report_2_geojson(track))
+    else:
+        abort(404)
+
+@app.route('/api/tracks/<mmsi>/history')
+def aShipHistory(mmsi):
+    history =  HistoricPositionReport.query.filter(HistoricPositionReport.mmsi == int(mmsi)).all()
+    geojson_coordinates = [[x.lat, x.long] for x in history]
+    return jsonify({'type' : 'Feature', 'geometry' : {'type' : 'LineString', 'coordinates' : geojson_coordinates}})
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
