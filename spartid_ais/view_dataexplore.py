c1254571a2407d0fecb29548e038fdf972a2ea01
Knut H Pederseen <knuthp@gmail.com>
2023-11-24 22:38:42 +0100

    dataexplore bootstrap and vessels data
    Simple explorer of vessels and vessels travel.
    Try streamlit before continue on this.

--- spartid_ais/view_dataexplore.py
+++ spartid_ais/view_dataexplore.py
@@ -7,9 +7,29 @@ bp = Blueprint("dataexplore", __name__)
 
 @bp.route("/simplestats")
 def simple_stats():
+    return "TBD"
+
+
+@bp.route("/vessels/<int:mmsi>/travel")
+def vessel_travel(mmsi):
+    df_travel_raw = pd.read_sql_query(
+        """
+            SELECT *
+            FROM historic_position
+            WHERE mmsi = %(mmsi)s AND
+                timestamp > now() - interval '3 day';
+        """,
+        db.engine,
+        params={"mmsi": mmsi},
+    )
+    return render_template("vessel_travel.html.j2", datatable=df_travel_raw.to_html())
+
+
+@bp.route("/vessels")
+def known_vessels():
     df_raw_imo_vessels = pd.read_sql("SELECT * from imo_vessel_codes", db.engine)
     return render_template(
-        "statistics.html.j2",
+        "vessels.html.j2",
         datatable=df_raw_imo_vessels.to_html(
             classes=["table table-bordered", "table-striped", "table-hover"]
         ),
