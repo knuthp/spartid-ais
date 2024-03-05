0282a1a2da71d0af3b3209437077f2c1bff6049f
Knut H Pedersen <knuthp@gmail.com>
2024-03-05 21:40:01 +0100

    Restrict AIS data to geolocation with geojson
    Minimal viable solution
    Allows one feature, does not check input
    Fixes #4

--- spartid_ais/views.py
+++ spartid_ais/views.py
@@ -1,6 +1,8 @@
 import datetime
 
-from flask import abort, jsonify, render_template, Blueprint
+from flask import abort, jsonify, render_template, request, Blueprint
+import duckdb
+from shapely.geometry import shape
 from spartid_ais.models import (
     HistoricPositionReport,
     ImoVesselCodes,
@@ -30,6 +32,19 @@ def _last_position_report_2_geojson(x):
     }
 
 
+def _last_position_report_2_geojson(x):
+    return {
+        "type": "Feature",
+        "properties": {
+            "mmsi": x.mmsi,
+            "course": x.course,
+            "heading": x.heading,
+            "speed": x.speed,
+            "timestamp": x.timestamp,
+        },
+        "geometry": {"type": "Point", "coordinates": [x.long, x.lat]},
+    }
+
 @bp.route("/api/tracks")
 def aTracks():
     six_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(hours=6)
@@ -39,6 +54,42 @@ def aTracks():
     print("Current number of tracks: {}".format(len(tracks)))
     geojson_features = [_last_position_report_2_geojson(x) for x in tracks]
     return jsonify({"type": "FeatureCollection", "features": geojson_features})
+
+
+@bp.route("/api/tracks/within_geojson", methods=["POST"])
+def tracks_within_geojson():
+    geojson_data = request.json
+    con = duckdb.connect()
+    for ext in ["postgres", "spatial"]:
+        con.install_extension(ext)
+        con.load_extension(ext)
+    con.sql("""
+        ATTACH 'dbname=spartid_ais user=postgres password=postgres host=127.0.0.1' AS db (TYPE postgres); 
+    """)
+    feature = geojson_data
+    area = shape(feature["features"][0]["geometry"])
+    # Execute the SQL query to 
+    query = """
+        SELECT *, ST_POINT(long, lat) as geom, datediff('hour', now() AT TIME ZONE 'UTC', timestamp)
+        FROM db.public.last_position
+        WHERE ST_INTERSECTS(geom, (?)) AND datediff('hour', now()::timestamp, timestamp) > -6
+    """
+
+    df = con.execute(query, (area.wkt,)).df()
+    ret = []
+    for _, row in df.iterrows():
+        ret.append({
+            "type": "Feature",
+            "properties": {
+                "mmsi": row["mmsi"],
+                "course": row["course"],
+                "heading": row["heading"],
+                "speed": row["speed"],
+                "timestamp": row["timestamp"],
+            },
+            "geometry": {"type": "Point", "coordinates": [row["long"], row["lat"]]},
+        })    
+    return jsonify({"type": "FeatureCollection", "features": ret})
 
 
 @bp.route("/api/tracks/<mmsi>")
